5d76adc2b7f655bdc2a61cada09ce2730b2158d0
Baitinq <manuelpalenzuelamerino@gmail.com>
2024-04-28 23:25:49 +0200

    Initial commit

---
+++ src/rest-api/target/debug/deps/rest_api-21ca80b7de66cea6.d
@@ -0,0 +1,5 @@
+/home/baitinq/src/fs-tracer-backend/src/rest-api/target/debug/deps/rest_api-21ca80b7de66cea6: src/main.rs
+
+/home/baitinq/src/fs-tracer-backend/src/rest-api/target/debug/deps/rest_api-21ca80b7de66cea6.d: src/main.rs
+
+src/main.rs:
---
+++ src/rest-api/target/debug/rest-api.d
@@ -0,0 +1,1 @@
+/home/baitinq/src/fs-tracer-backend/src/rest-api/target/debug/rest-api: /home/baitinq/src/fs-tracer-backend/src/rest-api/src/main.rs
