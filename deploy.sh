503c8e5333d979d92e2dbc7670acddfd5475d892
Baitinq <manuelpalenzuelamerino@gmail.com>
2024-05-25 17:59:25 +0200

    misc: deploy in parallel

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-bazel run //k8s/rest-api:chart.upgrade
-bazel run //k8s/payload-processor:chart.upgrade
+bazel run //k8s/rest-api:chart.upgrade &
+bazel run //k8s/payload-processor:chart.upgrade &
