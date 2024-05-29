36b8a4a0b2fb7ca73e5702a0e1a4594432ccdd48
Baitinq <manuelpalenzuelamerino@gmail.com>
2024-05-29 22:31:52 +0200

    misc: Rename API_KEY env var to FS_TRACER_API_KEY

--- requests_examples.sh
+++ requests_examples.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-curl -H "API_KEY: ${API_KEY}" -X POST -d '
+curl -H "API_KEY: ${FS_TRACER_API_KEY}" -X POST -d '
 {
 	"timestamp": "2020-01-02T15:04:05Z",
 	"absolute_path": "/home/user/file.txt",
@@ -8,5 +8,5 @@ curl -H "API_KEY: ${API_KEY}" -X POST -d '
 }
 ' http://leunam.dev:9999/file/
 
-curl -H "API_KEY: ${API_KEY}" -X GET http://leunam.dev:9999/file/?path=%2Fhome%2Fuser%2Ffile.txt
+curl -H "API_KEY: ${FS_TRACER_API_KEY}" -X GET http://leunam.dev:9999/file/?path=%2Fhome%2Fuser%2Ffile.txt
 
