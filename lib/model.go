51fa14eb787c1866271d61ac3302e0aac899724f
Baitinq <manuelpalenzuelamerino@gmail.com>
2024-07-20 13:29:26 +0200

    Lib: File: Update file to use UUID as primary key

--- lib/model.go
+++ lib/model.go
@@ -3,7 +3,7 @@ package lib
 import "time"
 
 type File struct {
-	Id            int       `db:"id"`
+	Id            string    `db:"id"`
 	User_id       string    `db:"user_id"`
 	Absolute_path string    `db:"absolute_path"`
 	Contents      string    `db:"contents"`
