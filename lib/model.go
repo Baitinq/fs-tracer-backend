78bb3d999c5d1016dc83e97501fae208c7b83b0c
Baitinq <manuelpalenzuelamerino@gmail.com>
2024-08-19 20:18:49 +0200

    Misc: Serialize file to json in lowercase

--- lib/model.go
+++ lib/model.go
@@ -3,9 +3,9 @@ package lib
 import "time"
 
 type File struct {
-	Id            string    `db:"id"`
-	User_id       string    `db:"user_id"`
-	Absolute_path string    `db:"absolute_path"`
-	Contents      string    `db:"contents"`
-	Timestamp     time.Time `db:"timestamp"`
+	Id            string    `db:"id" json:"id"`
+	User_id       string    `db:"user_id" json:"user_id"`
+	Absolute_path string    `db:"absolute_path" json:"absolute_path"`
+	Contents      string    `db:"contents" json:"contents"`
+	Timestamp     time.Time `db:"timestamp" json:"timestamp"`
 }
